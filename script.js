77d7ea5bf4e96013b7f7081bbd1d96c8fd6266a6
Akshayjain-555 <akshayjain0522@gmail.com>
2025-07-06 17:26:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function openPopup() {
-  document.getElementById("consultation-popup").style.display = "flex";
-}
 
-function closePopup() {
-  document.getElementById("consultation-popup").style.display = "none";
-}
-
-document.getElementById("consultation-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Your consultation request has been submitted!");
-  closePopup();
-});
